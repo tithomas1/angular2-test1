791e797192e51c2017919c1beaead4e1f555746b
tithomas1 <tithomas@cisco.com>
2016-07-26 16:58:44 -0400

    Correct small typo

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,6 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'test-tag',
-    template: '<h1>Succesful test of Angular2!</h1>'
+    template: '<h1>Simple Angular2 component appears to be working</h1>'
 })
 export class AppComponent { }
